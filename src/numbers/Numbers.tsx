6fe7b29aa4d88f0ca941a12f5b2d7e9c2dd82c25
Chris Cox <c.cox4218@gmail.com>
2026-02-16 22:57:35 -0500

    planning doc and stylized header and title

--- src/numbers/Numbers.tsx
+++ src/numbers/Numbers.tsx
@@ -3,11 +3,20 @@ import './numbers.css';
 function Numbers() {
     return (
         <>
-            <div className='bg'></div>
-            <div className='header-text'>
-                <div>FINAL</div>
-                <div>EXERCISES</div>
+            <div className='header'>
+                <div className='header-image'>
+                    <img src='src/assets/FE24ByTheNumbers/Header_SF_FEBytheNumbers_DA_JD.jpg'></img>
+                </div>
+                <div className='title'>
+                    <div className='title-text'>FINAL</div>
+                    <div className='title-text'>EXERCISES</div>
+                    <div className='by-the-numbers'>           
+                        <div className='year-text'>2024</div>
+                        <span className='numbers-text'>BY THE NUMBERS</span>
+                    </div>
+                </div>
             </div>
+            <div className='connecting-line'></div>
         </>
     )
 }
---
+++ src/scratch/planning.md
@@ -0,0 +1,30 @@
+# Components
+
+- Header
+- Degrees Statistic
+- Degrees by School chart
+  - Pie chart in PSD file - line? bar?
+  - MUI X Library
+  - Show stats on hover, "pin" on click?
+- Graduate and Professional Degrees
+- 2 Year Degrees
+-  1st Gen Degrees
+- Graduate Ages
+- Sustainability Stats
+- Footer
+
+
+
+# Work stages
+
+1. Research
+   - Decide on tooling
+   - investigate animations
+   - sketch ideas
+   - brainstorm
+2. Static Version
+   - build out as close to PSD spec as possible, no animations
+   - Decide on stylistic changes as needed
+3. Animate
+   -  Make it pretty
+   -  touch up styling to suit animations
