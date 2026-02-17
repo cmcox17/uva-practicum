646c5daaaaff73cdbe203650958a13a3c72761ea
Chris Cox <c.cox4218@gmail.com>
2026-02-17 00:41:14 -0500

    Revert "installed sass for styling rules to apply properly, implemented unclean grad degree section"
    This reverts commit 99851dc281247e9a129113af0aab92557ba6cf19.

--- src/numbers/Numbers.tsx
+++ src/numbers/Numbers.tsx
@@ -1,4 +1,4 @@
-import './numbers.scss';
+import './numbers.css';
 
 function Numbers() {
     return (
@@ -20,44 +20,11 @@ function Numbers() {
             <div className='degree-image'>
                 <img src='src/assets/FE24ByTheNumbers/FE24ByTheNumbers_Stat_1_Circle.png'></img>
             </div>
-            <div className='degree-section'>
-                <div className='degree-number franklin-gothic-demi'>7616</div>
-                <div className='degree-text franklin-gothic-medium'>Total Number of Degrees Awarded</div>
-                <hr className='degree-break'></hr>
-                <div className='degree-number franklin-gothic-demi'>4394</div>
-                <div className='degree-text franklin-gothic-medium'>Total Number of Baccalaureate Degrees</div>
-                <div className='connecting-line'></div>
-                <div className='degree-chart'>
-                    {/* TODO: Implement MUI X Pie Chart with Degree Data */}
-                    Placeholder!
-                </div>
-                <div className='connecting-line'></div>
-                <div className='degree-text franklin-gothic-medium'>Graduate and Professional Degrees</div>
-                <div className='graduate-degrees-section'>
-                    {/* TODO: Align these properly */}
-                    <div className='graduate-degrees-subsection'>
-                        <div className='graduate-degrees-image'>
-                            <img src='src/assets/FE24ByTheNumbers/FE24ByTheNumbers_PhD_Icon.png'></img>
-                        </div>
-                        <div className='graduate-degrees-number franklin-gothic-demi'>436</div>
-                        <div className='graduate-degrees-text franklin-gothic-medium'>Ph.Ds</div>
-                    </div>
-                    <div className='graduate-degrees-subsection'>
-                        <div className='graduate-degrees-image'>
-                            <img src='src/assets/FE24ByTheNumbers/FE24ByTheNumbers_JD_Icon.png'></img>
-                        <div className='graduate-degrees-number franklin-gothic-demi'>436</div>
-                        <div className='graduate-degrees-text franklin-gothic-medium'>JDs</div>
-                        </div>
-                    </div>
-                    <div className='graduate-degrees-subsection'>
-                        <div className='graduate-degrees-image'>
-                            <img style={{width: '335px', height: '382px'}} src='src/assets/FE24ByTheNumbers/FE24ByTheNumbers_MD_Icon.png'></img>
-                        <div className='graduate-degrees-number franklin-gothic-demi'>436</div>
-                        <div className='graduate-degrees-text franklin-gothic-medium'>MDs</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <div className='degree-number franklin-gothic-demi'>7616</div>
+            <div className='degree-text franklin-gothic-medium'>Total Number of Degrees Awarded</div>
+            <hr className='degree-break'></hr>
+            <div className='degree-number franklin-gothic-demi'>4394</div>
+            <div className='degree-text franklin-gothic-medium'>Total Number of Baccalaureate Degrees</div>
         </>
     )
 }
