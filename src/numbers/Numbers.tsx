9d2fed00a550effe7969187225f06e76c9d55e01
Chris Cox <c.cox4218@gmail.com>
2026-02-16 23:19:04 -0500

    built some utility classes and degree section

--- src/numbers/Numbers.tsx
+++ src/numbers/Numbers.tsx
@@ -8,15 +8,23 @@ function Numbers() {
                     <img src='src/assets/FE24ByTheNumbers/Header_SF_FEBytheNumbers_DA_JD.jpg'></img>
                 </div>
                 <div className='title'>
-                    <div className='title-text'>FINAL</div>
-                    <div className='title-text'>EXERCISES</div>
+                    <div className='title-text franklin-gothic-demi'>FINAL</div>
+                    <div className='title-text franklin-gothic-demi'>EXERCISES</div>
                     <div className='by-the-numbers'>           
                         <div className='year-text'>2024</div>
-                        <span className='numbers-text'>BY THE NUMBERS</span>
+                        <span className='numbers-text franklin-gothic-book'>BY THE NUMBERS</span>
                     </div>
                 </div>
             </div>
             <div className='connecting-line'></div>
+            <div className='degree-image'>
+                <img src='src/assets/FE24ByTheNumbers/FE24ByTheNumbers_Stat_1_Circle.png'></img>
+            </div>
+            <div className='degree-number franklin-gothic-demi'>7616</div>
+            <div className='degree-text franklin-gothic-medium'>Total Number of Degrees Awarded</div>
+            <hr className='degree-break'></hr>
+            <div className='degree-number franklin-gothic-demi'>4394</div>
+            <div className='degree-text franklin-gothic-medium'>Total Number of Baccalaureate Degrees</div>
         </>
     )
 }
