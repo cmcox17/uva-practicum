da3633eb915440cec5283fc0522abd2ebbb2ed4e
Chris Cox <c.cox4218@gmail.com>
2026-02-18 19:38:30 -0500

    some responsiveness for mobile viewing of home page and transcript

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ function App() {
 
   return (
     <>
-      <div className='header'>
+      <div className='desktop-header'>
         <h1>
           UVA Web Developer Practicum
         </h1>
       </div>
+      <div className='mobile-header'>
+        <h3>UVA Web Developer Practicum</h3>
+      </div>
       <div className='page-links'>
         <button>
           <Link to="./uva-practicum/transcript" target='_self'>
-            <h1>Transcript</h1>
+            <h2>Transcript</h2>
           </Link>
         </button>
         <button>
           <Link to="./uva-practicum/numbers" target="_self">
-            <h1>2024 By The Numbers</h1>
+            <h2>2024 By The Numbers</h2>
           </Link>
         </button>
       </div>
