228eb5e424599a5c67bb29f684bf676490760fa2
Chris Cox <c.cox4218@gmail.com>
2026-02-19 01:31:21 -0500

    first pass at dark mode styling

--- src/numbers/UndergradPieChart.tsx
+++ src/numbers/UndergradPieChart.tsx
@@ -23,8 +23,9 @@ const StyledNumber = styled('text')(() => {
 
 const StyledText = styled('text')(() => {
     const isSmall = useMediaQuery('(max-width: 990px)');
+    const isDarkMode = useMediaQuery('(prefers-color-scheme: dark')
     return {
-        fill: '#232d4b',
+        fill: isDarkMode ? 'white' : '#232d4b',
         textAnchor: 'middle',
         dominantBaseline: 'central',
         fontSize: isSmall ? '1.5rem' : '2rem',
